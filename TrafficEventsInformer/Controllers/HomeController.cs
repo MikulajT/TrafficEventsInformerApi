0145fed9760884eb6c437c9915b026e0e337cad3
Tomas Mikulaj <mikulajt@gmail.com>
2024-05-25 12:11:15 +0200

    Add empty route endpoint

---
+++ TrafficEventsInformer/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrafficEventsInformer.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return Ok("API is running.");
+        }
+    }
+}
