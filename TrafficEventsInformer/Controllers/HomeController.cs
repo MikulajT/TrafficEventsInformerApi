8847b43b683f9d8de66275509dd321303bde2d55
Tomas Mikulaj <mikulajt@gmail.com>
2024-09-11 21:33:30 +0200

    Add SQLite DB file download

--- TrafficEventsInformer/Controllers/HomeController.cs
+++ TrafficEventsInformer/Controllers/HomeController.cs
@@ -3,15 +3,31 @@
 namespace TrafficEventsInformer.Controllers
 {
     [ApiController]
-    [ApiExplorerSettings(IgnoreApi = true)]
+    //[ApiExplorerSettings(IgnoreApi = true)]
     public class HomeController : ControllerBase
     {
 
+        [ApiExplorerSettings(IgnoreApi = true)]
         [HttpGet]
         [Route("")]
         public IActionResult Index()
         {
             return Ok("API is running.");
+        }
+
+        [HttpGet]
+        [Route("download-db")]
+        public IActionResult DownloadSQLiteDb()
+        {
+            var dbPath = Path.Combine(Directory.GetCurrentDirectory(), "tei.db");
+
+            if (System.IO.File.Exists(dbPath))
+            {
+                var fileBytes = System.IO.File.ReadAllBytes(dbPath);
+                return File(fileBytes, "application/octet-stream", "tei.db");
+            }
+
+            return NotFound("Database file not found");
         }
     }
 }
