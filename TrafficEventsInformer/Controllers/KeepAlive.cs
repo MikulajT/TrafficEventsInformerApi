b1d09c499f11dc7d6fc15c63ccf97c4c253505e7
Tomas Mikulaj <mikulajt@gmail.com>
2024-11-30 10:42:02 +0100

    Add keep alive endpoint

---
+++ TrafficEventsInformer/Controllers/KeepAlive.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrafficEventsInformer.Controllers
+{
+    public class KeepAlive : ControllerBase
+    {
+
+        /// <summary>
+        /// Endpoint is being called by external service to keep app alive on hosting site
+        /// </summary>
+        /// <returns></returns>
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [HttpGet]
+        [Route("api/keep-alive")]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
